a601fb0e85a3889fa3e1998d4e125ca9b8fff5af
Maskbor <Maskbor@gmail.com>
2021-01-24 11:36:31 +0300

    Добавьте файлы проекта.

---
+++ TodoAPI/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+
+}
